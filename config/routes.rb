bb64c42b386ba9210fc47e5334f0fda3c5a083ff
Felix Clack <felixclack@gmail.com>
2023-02-17 14:00:32 +0000

    Add a support interface
    We want an interface to be able to manage the feature flags for the
    service.
    
    Basic auth is suitable for the first iteration of authorization.

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,7 @@
-Rails.application.routes.draw { root to: "pages#home" }
+Rails.application.routes.draw do
+  root to: "pages#home"
+
+  namespace :support_interface, path: "/support" do
+    get "/", to: "support_interface#index"
+  end
+end
