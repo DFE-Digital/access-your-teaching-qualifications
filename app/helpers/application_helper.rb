7380954e5dd6e9dbafedbbedfa72e25d97115725
Felix Clack <felixclack@gmail.com>
2023-02-20 09:57:03 +0000

    Add feature flags library (#15)
    We want to be able to feature flag new code to be able to better control
    how we release new things.
    
    The govuk_feature_flags library is in use in other projects (Find +
    Refer) and has proven to be a simple way of adding this functionality.
    
    This change includes an update to the layouts in order to be able to
    display a separate navigation for the support section versus the public
    view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def current_namespace
+    request.path.split("/").second
+  end
+
+  def navigation
+    govuk_header(service_name: t("service.name")) do |header|
+      case current_namespace
+      when "support"
+        header.navigation_item(
+          active: current_page?(main_app.support_interface_feature_flags_path),
+          href: main_app.support_interface_feature_flags_path,
+          text: "Features"
+        )
+      end
+    end
+  end
 end
