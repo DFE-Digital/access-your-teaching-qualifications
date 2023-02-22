866949cb633820a0ebc73b02d64f73d1515a5864
Malcolm Baig <git@baig.org>
2023-02-22 13:38:51 +0000

    Add sign_in controller
    The #new action in this controller gives us a starting point for the Get
    an identity auth flow.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
   devise_for :staff,
              controllers: {
                confirmations: "staff/confirmations",
@@ -8,18 +10,17 @@ Rails.application.routes.draw do
                unlocks: "staff/unlocks"
              }
 
-  devise_for :users,
-             controllers: {
-               omniauth_callbacks: "users/omniauth_callbacks"
-             }
-  root to: "pages#home"
-
   namespace :support_interface, path: "/support" do
     get "/", to: "support_interface#index"
 
     mount FeatureFlags::Engine => "/features"
   end
 
+  devise_for :users,
+             controllers: {
+               omniauth_callbacks: "users/omniauth_callbacks"
+             }
+  get "/sign-in", to: "users/sign_in#new"
   get "/accessibility", to: "static#accessibility"
   get "/cookies", to: "static#cookies"
   get "/privacy", to: "static#privacy"
