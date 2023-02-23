f1e8bdd85811643ffb1fdcd27013af88f877c94e
Malcolm Baig <git@baig.org>
2023-02-23 14:05:07 +0000

    Add database fields to capture Identity info on User
    - Migration to add the initial set of fields capturing name, DoB, and
      TRN
    - A method on User to find_or_initialize by email and populate this
      data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   devise :omniauthable
+
+  def self.from_identity(auth_data)
+    email = auth_data.info.email
+    user = find_or_initialize_by(email:)
+    user.assign_attributes(
+      name: auth_data.info.name,
+      given_name: auth_data.info.given_name,
+      family_name: auth_data.info.family_name,
+      trn: auth_data.info.trn,
+      date_of_birth: auth_data.info.date_of_birth
+    )
+    user.tap(&:save!)
+  end
 end
