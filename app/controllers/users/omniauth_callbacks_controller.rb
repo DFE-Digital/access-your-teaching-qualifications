3fdce395f3d3d9bbcf004f725e2004155517737f
Malcolm Baig <git@baig.org>
2023-03-01 09:44:50 +0000

    Add basic failure handling and debug info to user sign in
    - If something goes wrong during the Identity auth flow, redirect back
      to the sign in screen
    - Print the auth credentials to the log when in development mode, to
      help with debugging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_identity(auth)
     sign_in @user
     session[:identity_user_token] = auth.credentials.token
+    session[:identity_user_token_expiry] = auth.credentials.expires_at
+    log_auth_credentials_in_development(auth)
     flash[:notice] = "Signed in successfully."
     redirect_to root_path
   end
@@ -17,14 +19,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
-  # def failure
-  #   super
-  # end
+  def failure
+    flash[:warning] = "There was a problem. Please try signing in again"
+    super
+  end
 
-  # protected
+  protected
 
   # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def after_omniauth_failure_path_for(_scope)
+    sign_in_path
+  end
+
+  private
+
+  def log_auth_credentials_in_development(auth)
+    if Rails.env.development?
+      Rails.logger.debug auth.credentials.token
+      Rails.logger.debug Time.zone.at auth.credentials.expires_at
+    end
+  end
 end
