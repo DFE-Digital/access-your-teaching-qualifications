92aaef6e92b29837e9cc840bd8f2fb85ef8d3a12
Malcolm Baig <git@baig.org>
2023-03-01 11:56:32 +0000

    Add adapter for Qualifications API
    The QualificationsApi::Client acts as an adapter class for the
    Qualifications API. Its initial implementation wraps the teacher
    endpoint, returning a Ruby object populated with data from the API call.

---
+++ app/lib/qualifications_api/client.rb
@@ -0,0 +1,31 @@
+module QualificationsApi
+  class Client
+    TIMEOUT_IN_SECONDS = 5
+
+    attr_reader :token
+
+    def initialize(token:)
+      @token = token
+    end
+
+    def teacher
+      response = client.get("v3/teacher")
+      QualificationsApi::Teacher.new response.body
+    end
+
+    def client
+      @client ||=
+        Faraday.new(
+          url: ENV.fetch("QUALIFICATIONS_API_URL"),
+          request: {
+            timeout: TIMEOUT_IN_SECONDS
+          }
+        ) do |faraday|
+          faraday.request :authorization, "Bearer", token
+          faraday.request :json
+          faraday.response :json
+          faraday.adapter Faraday.default_adapter
+        end
+    end
+  end
+end
