9cb8eb1b1ab7d6cb06e622a6a703b4f648b5ddfe
Malcolm Baig <git@baig.org>
2023-03-02 21:50:21 +0000

    Amend token retrieval in Identity auth strategy
    Something's changed in the Identity callback request that's altered how
    the token is provided. Amend our Devise auth strategy accordingly.

--- lib/omniauth/strategies/identity.rb
+++ lib/omniauth/strategies/identity.rb
@@ -28,7 +28,7 @@ module Omniauth
 
       extra { { "raw_info" => raw_info } }
 
-      credentials { { token: access_token } }
+      credentials { { token: access_token.token } }
 
       def raw_info
         @raw_info ||= access_token.get("connect/userinfo").parsed
