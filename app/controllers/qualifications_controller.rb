dcdb82cca90bd4975bfae1574b727fb8a929fc3b
Malcolm Baig <git@baig.org>
2023-03-08 13:27:58 +0000

    Add basic starting point for auth journey
    The design proposed in the prototype has the starting point for the
    Qualifications auth journey be either the GOVUK start page or an
    interstitial page on Identity. Neither of these are currently built.
    
    For now, have the root path of the service be a rudimentary sign in
    page. This commit also includes before_action filters to ensure users
    are correctly redirected to either the sign in page or the
    qualifications screen depending on their auth state.

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -1,4 +1,6 @@
 class QualificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user =
       current_user ||
--- app/controllers/users/sign_in_controller.rb
+++ app/controllers/users/sign_in_controller.rb
@@ -1,4 +1,10 @@
 class Users::SignInController < ApplicationController
+  before_action :redirect_to_qualifications, if: :user_signed_in?
+
   def new
+  end
+
+  def redirect_to_qualifications
+    redirect_to qualifications_path
   end
 end
