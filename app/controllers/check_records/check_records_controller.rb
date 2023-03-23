c2b57c2cbba9a526102cedbeda30d4027a7b880d
Malcolm Baig <git@baig.org>
2023-03-23 14:01:07 +0000

    Add layout and navigation for Check records service
    - Use a separate layout file for the Check records service so that its
      UI can be iterated on separately from Access your teaching
      qualifications
    - Use this layout in all controllers inheriting from
      CheckRecords::CheckRecordsController
    - Add a component for the Check records navigation

--- app/controllers/check_records/check_records_controller.rb
+++ app/controllers/check_records/check_records_controller.rb
@@ -1,4 +1,5 @@
 module CheckRecords
   class CheckRecordsController < ApplicationController
+    layout "check_records_layout"
   end
 end
---
+++ spec/system/check_records/user_view_check_records_homepage_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "Check records homepage" do
+  include CommonSteps
+
+  scenario "User views Check records homepage" do
+    given_the_service_is_open
+    when_i_visit_the_check_records_homepage
+    then_i_see_the_check_records_nav
+  end
+
+  private
+
+  def when_i_visit_the_check_records_homepage
+    visit check_records_root_path
+  end
+
+  def then_i_see_the_check_records_nav
+    within("header") { expect(page).to have_content("Check the record of a teacher in England") }
+  end
+end
