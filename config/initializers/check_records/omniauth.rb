2d0e0102c5d2d72ece7b48da7008aea435586278
Malcolm Baig <git@baig.org>
2023-03-30 14:10:24 +0100

    Add config for Check records service DSI
    We're going to use DfE Sign In to authenticate users of the 'Check the
    record of a teacher' service.
    
    This commit adds some basic config and outlines the environment
    variables we'll need.

---
+++ config/initializers/check_records/omniauth.rb
@@ -0,0 +1,33 @@
+require "check_records/dfe_sign_in"
+
+OmniAuth.config.logger = Rails.logger
+
+dfe_sign_in_identifier = ENV.fetch("DFE_SIGN_IN_CLIENT_ID", "example")
+dfe_sign_in_secret = ENV.fetch("DFE_SIGN_IN_SECRET", "example")
+dfe_sign_in_redirect_uri = ENV.fetch("DFE_SIGN_IN_REDIRECT_URL", "example")
+dfe_sign_in_issuer_uri = ENV["DFE_SIGN_IN_ISSUER"].present? ? URI(ENV["DFE_SIGN_IN_ISSUER"]) : nil
+
+options = {
+  name: :dfe,
+  discovery: true,
+  response_type: :code,
+  scope: %i[email profile],
+  path_prefix: "/auth",
+  callback_path: "/auth/dfe/callback",
+  client_options: {
+    port: dfe_sign_in_issuer_uri&.port,
+    scheme: dfe_sign_in_issuer_uri&.scheme,
+    host: dfe_sign_in_issuer_uri&.host,
+    identifier: dfe_sign_in_identifier,
+    secret: dfe_sign_in_secret,
+    redirect_uri: dfe_sign_in_redirect_uri&.to_s
+  }
+}
+
+if CheckRecords::DfESignIn.bypass?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :developer, fields: %i[uid email first_name last_name], uid_field: :uid
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Strategies::OpenIDConnect, options
+end
