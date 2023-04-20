728bf56a81fdf3edeb122f57978758b0197065c6
Malcolm Baig <git@baig.org>
2023-04-20 13:28:29 +0100

    Add config check to dfe_bypass controller action
    Check that we're in bypass mode when processing this action. Redirect to
    check records root if not.

--- app/controllers/check_records/omniauth_callbacks_controller.rb
+++ app/controllers/check_records/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class CheckRecords::OmniauthCallbacksController < ApplicationController
   protect_from_forgery except: :dfe_bypass
 
   def dfe_bypass
+    redirect_to check_records_root_path unless CheckRecords::DfESignIn.bypass?
     @dsi_user = DsiUser.create_or_update_from_dsi(request.env["omniauth.auth"])
     @dsi_user.begin_session!(session)
 
