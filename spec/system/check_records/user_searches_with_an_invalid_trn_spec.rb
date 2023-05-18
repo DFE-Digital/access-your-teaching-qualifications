a6b428ef2d6134a6498f90233689c1f07817332a
Malcolm Baig <git@baig.org>
2023-05-18 21:55:33 +0100

    Fix stray linting error in system spec

--- spec/system/check_records/user_searches_with_an_invalid_trn_spec.rb
+++ spec/system/check_records/user_searches_with_an_invalid_trn_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe "TRN search", host: :check_records, type: :system do
   include ActivateFeaturesSteps
   include CheckRecords::AuthenticationSteps
 
-  scenario "User searches with invalid TRN",
-           test: %i[with_stubbed_auth with_fake_quals_api] do
+  scenario "User searches with invalid TRN", test: %i[with_stubbed_auth with_fake_quals_api] do
     given_the_service_is_open
     when_i_sign_in_via_dsi
     and_search_with_an_invalid_trn
