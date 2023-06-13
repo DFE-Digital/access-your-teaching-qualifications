82adf5b8f5ffc9c9d7bd84d309cf786df09718f9
Malcolm Baig <git@baig.org>
2023-06-13 11:39:54 +0100

    Fix AYTQ static content
    Some file renaming and controller inheritance to:
    
    - Render the static content pages properly
    - Allow access to the content when not signed in

--- app/controllers/qualifications/static_controller.rb
+++ app/controllers/qualifications/static_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 module Qualifications
-  class StaticController < ApplicationController
+  class StaticController < QualificationsInterfaceController
+    skip_before_action :authenticate_user!
+    skip_before_action :handle_expired_token!
+
     layout "two_thirds"
   end
 end
