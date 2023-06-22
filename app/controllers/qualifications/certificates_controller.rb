f7661f5a17ac6ba3844e413489413b2dcd71db48
Malcolm Baig <git@baig.org>
2023-06-22 14:45:40 +0100

    Add user name to certificate download file name
    Do the following:
    
    - Add a Certificate model to QualificationsApi to hold the file data
      and to generate the file name from arguments passed to the initializer
    - Have the Client return an instance of this model rather than the raw
      certificate data
    - Prepare the file download in the controller by using methods on the
      returned instance
    - Update system specs to confirm the file name of various downloaded
      certificate types

--- app/controllers/qualifications/certificates_controller.rb
+++ app/controllers/qualifications/certificates_controller.rb
@@ -2,9 +2,11 @@ module Qualifications
   class CertificatesController < QualificationsInterfaceController
     def show
       client = QualificationsApi::Client.new(token: session[:identity_user_token])
+      certificate =
+        client.certificate(name: current_user.name, type: params[:id], id: params[:certificate_id])
 
-      send_data client.certificate(type: params[:id], id: params[:certificate_id]),
-                name: "#{params[:type]}_certificate.pdf",
+      send_data certificate.file_data,
+                filename: certificate.file_name,
                 content_type: "application/pdf"
     end
   end
