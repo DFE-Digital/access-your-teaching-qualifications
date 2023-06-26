6467051bd5f7134148a3478a6f2ce137763d49e5
Malcolm Baig <git@baig.org>
2023-06-26 11:26:45 +0100

    Avoid using user-submitted value in Qualifications API call
    When retrieving a certificate form the API, the type is specified by a
    value submitted on the frontend.
    
    Add a layer of validation in the controller which queries a list of
    recognised types. This addresses server-side request forgery security
    warning.

--- app/controllers/qualifications/certificates_controller.rb
+++ app/controllers/qualifications/certificates_controller.rb
@@ -3,11 +3,28 @@ module Qualifications
     def show
       client = QualificationsApi::Client.new(token: session[:identity_user_token])
       certificate =
-        client.certificate(name: current_user.name, type: params[:id], id: params[:certificate_id])
+        client.certificate(
+          name: current_user.name,
+          type: certificate_type,
+          id: params[:certificate_id]
+        )
 
       send_data certificate.file_data,
                 filename: certificate.file_name,
                 content_type: "application/pdf"
+    end
+
+    private
+
+    def certificate_type
+      if params[:id].to_sym.in? QualificationsApi::Certificate::VALID_TYPES
+        params[:id]
+      else
+        raise UnrecognisedCertificateTypeError
+      end
+    end
+
+    class UnrecognisedCertificateTypeError < StandardError
     end
   end
 end
