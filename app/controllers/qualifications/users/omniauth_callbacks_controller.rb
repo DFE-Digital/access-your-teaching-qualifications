da0e478507b0060634e7a7b0882654fd511d7e46
Felix Clack <felixclack@gmail.com>
2023-07-06 09:32:18 +0100

    Add OIDC strategy for Identity
    The Identity service is an OpenID Connect provider. Changing the
    strategy we use with OmniAuth can make this more obvious.
    
    We currently have implemented the OAuth2 strategy for Identity and
    extended it to take advantage of the OIDC logout feature.
    
    Switching to an OIDC strategy makes it more obvious what features the
    Identity service provides. It also, hopefully, makes things clearer for
    any future maintainer of the code.

--- app/controllers/qualifications/users/omniauth_callbacks_controller.rb
+++ app/controllers/qualifications/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ module Qualifications
         @user = User.from_identity(auth)
         session[:identity_user_id] = @user.id
         session[:identity_user_token] = auth.credentials.token
-        session[:identity_user_token_expiry] = auth.credentials.expires_at
+        session[:identity_user_token_expiry] = auth.credentials.expires_in.seconds.from_now.to_i
 
         log_auth_credentials_in_development(auth)
         redirect_to qualifications_dashboard_path
@@ -19,7 +19,7 @@ module Qualifications
       def log_auth_credentials_in_development(auth)
         if Rails.env.development?
           Rails.logger.debug auth.credentials.token
-          Rails.logger.debug Time.zone.at auth.credentials.expires_at
+          Rails.logger.debug Time.zone.at auth.credentials.expires_in
         end
       end
     end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,32 +1,9 @@
 require "check_records/dfe_sign_in"
-require "omniauth/strategies/identity"
 
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.on_failure = proc { |env| AuthFailuresController.action(:failure).call(env) }
+OmniAuth.config.on_failure =
+  proc { |env| AuthFailuresController.action(:failure).call(env) }
 
-# DSI setup
-dfe_sign_in_identifier = ENV["DFE_SIGN_IN_CLIENT_ID"]
-dfe_sign_in_secret = ENV["DFE_SIGN_IN_SECRET"]
-dfe_sign_in_redirect_uri = ENV["DFE_SIGN_IN_REDIRECT_URL"]
-dfe_sign_in_issuer_uri = ENV["DFE_SIGN_IN_ISSUER"].present? ? URI(ENV["DFE_SIGN_IN_ISSUER"]) : nil
-options = {
-  name: :dfe,
-  discovery: true,
-  response_type: :code,
-  scope: %i[email profile organisation],
-  path_prefix: "/check-records/auth",
-  callback_path: "/check-records/auth/dfe/callback",
-  client_options: {
-    port: dfe_sign_in_issuer_uri&.port,
-    scheme: dfe_sign_in_issuer_uri&.scheme,
-    host: dfe_sign_in_issuer_uri&.host,
-    identifier: dfe_sign_in_identifier,
-    secret: dfe_sign_in_secret,
-    redirect_uri: dfe_sign_in_redirect_uri&.to_s
-  },
-  issuer:
-    ("#{dfe_sign_in_issuer_uri}:#{dfe_sign_in_issuer_uri.port}" if dfe_sign_in_issuer_uri.present?)
-}
 if CheckRecords::DfESignIn.bypass?
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer,
@@ -35,13 +12,47 @@ if CheckRecords::DfESignIn.bypass?
              path_prefix: "/check-records/auth"
   end
 else
-  Rails.application.config.middleware.use OmniAuth::Strategies::OpenIDConnect, options
+  dfe_sign_in_issuer_uri = URI(ENV["DFE_SIGN_IN_ISSUER"])
+
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :openid_connect,
+             name: :dfe,
+             callback_path: "/check-records/auth/dfe/callback",
+             client_options: {
+               host: dfe_sign_in_issuer_uri&.host,
+               identifier: ENV["DFE_SIGN_IN_CLIENT_ID"],
+               port: dfe_sign_in_issuer_uri&.port,
+               redirect_uri: ENV["DFE_SIGN_IN_REDIRECT_URL"],
+               scheme: dfe_sign_in_issuer_uri&.scheme,
+               secret: ENV.fetch("DFE_SIGN_IN_SECRET", "example")
+             },
+             discovery: true,
+             issuer: "#{dfe_sign_in_issuer_uri}:#{dfe_sign_in_issuer_uri.port}",
+             path_prefix: "/check-records/auth",
+             response_type: :code,
+             scope: %i[email organisation profile]
+  end
 end
 
-# Identity setup
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :identity,
-           ENV["IDENTITY_CLIENT_ID"],
-           ENV["IDENTITY_CLIENT_SECRET"],
-           { path_prefix: "/qualifications/users/auth" }
+  provider :openid_connect,
+           name: :identity,
+           callback_path: "/qualifications/users/auth/identity/callback",
+           client_options: {
+             host: URI(ENV["IDENTITY_API_DOMAIN"]).host,
+             identifier: ENV["IDENTITY_CLIENT_ID"],
+             port: 443,
+             redirect_uri:
+               "#{ENV["HOSTING_DOMAIN"]}/qualifications/users/auth/identity/callback",
+             scheme: "https",
+             secret: ENV["IDENTITY_CLIENT_SECRET"]
+           },
+           discovery: true,
+           issuer: ENV["IDENTITY_API_DOMAIN"],
+           path_prefix: "/qualifications/users/auth",
+           pkce: true,
+           post_logout_redirect_uri:
+             "#{ENV["HOSTING_DOMAIN"]}/qualifications/sign-out",
+           response_type: :code,
+           scope: %w[email openid profile dqt:read]
 end
