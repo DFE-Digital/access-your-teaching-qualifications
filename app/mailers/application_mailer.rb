eb981ac3bf07545b1c5dbf4210a33ff4f1c247c5
Graham Pengelly <graham@three-tier.com>
2023-08-14 16:35:57 +0100

    Update Notify template IDs
    These have been set up in Notify now so needed updating.
    Also add mailer method override to Staff.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ApplicationMailer < Mail::Notify::Mailer
-  GENERIC_NOTIFY_TEMPLATE = "5e1842e6-d806-49ce-ba19-0483a056e367"
+  GENERIC_NOTIFY_TEMPLATE = "d5bddb2d-e560-4de1-a851-5e470bf06c76"
 
   def notify_email(headers)
     headers = headers.merge(rails_mailer: mailer_name, rails_mail_template: action_name)
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -10,4 +10,8 @@ class Staff < ApplicationRecord
     :trackable,
     :validatable
   )
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
