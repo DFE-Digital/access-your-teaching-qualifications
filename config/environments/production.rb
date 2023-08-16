247a3e69153bcfb02d42843c513b3a7471651b94
Graham Pengelly <graham@three-tier.com>
2023-08-16 14:26:30 +0100

    Add default_url_options[:host] for mail
    Notify emails are failing due to this not being set.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :notify
+  config.action_mailer.default_url_options = { host: HostingEnvironment.aytq_domain }
 end
