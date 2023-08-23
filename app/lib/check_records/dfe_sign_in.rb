6a9d5397db16d012b6e62f0ed2a94c8d0d48cdf4
Malcolm Baig <git@baig.org>
2023-08-23 13:22:01 +0100

    Add basic auth bypass to Check service review apps
    Repurpose the existing dev bypass and make it available in review apps
    as well.
    
    We can iterate on this and eventually introduce different kinds of user
    sign in. Currently we don't have any kind of permissions model to
    emulate, so here we just click through and sign in as a generic user.
    This gives full access to Check functionality.

--- app/lib/check_records/dfe_sign_in.rb
+++ app/lib/check_records/dfe_sign_in.rb
@@ -3,7 +3,15 @@ require "hosting_environment"
 module CheckRecords
   class DfESignIn
     def self.bypass?
+      bypass_active? || review_app?
+    end
+
+    def self.bypass_active?
       HostingEnvironment.test_environment? && ENV["BYPASS_DSI"] == "true"
+    end
+
+    def self.review_app?
+      HostingEnvironment.review?
     end
   end
 end
