6c819b5caea124f61e700ca3de06871e3197b6a3
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-08-29 16:27:11 +0100

    Add feedback form
    - Create new MVC for feedbacks
    - Add presence validations for fields
    - Send feedback details to bigquery
    - Update formbuilder initializer to render different error title

---
+++ app/controllers/check_records/feedbacks_controller.rb
@@ -0,0 +1,31 @@
+module CheckRecords
+  class FeedbacksController < CheckRecordsController
+    skip_before_action :authenticate_dsi_user!
+    skip_before_action :handle_expired_session!
+
+    def new
+      @feedback = Feedback.new
+    end
+
+    def create
+      @feedback = Feedback.new(feedback_params)
+
+      if @feedback.save
+        redirect_to check_records_success_path
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def feedback_params
+      params.require(:feedback).permit(
+        :satisfaction_rating,
+        :improvement_suggestion,
+        :contact_permission_given,
+        :email
+      )
+    end
+  end
+end
