d507c292cb5f9535dfaa1e3dd6c86a13c6a67556
Steve Laing <steve.laing@gmail.com>
2023-09-05 14:03:10 +0100

    Redirect user to sign in page on sign out

---
+++ spec/system/check_records/user_signs_out_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "DSI authentication", host: :check_records do
+  include AuthorizationSteps
+  include CheckRecords::AuthenticationSteps
+
+  scenario "User signs out", test: :with_stubbed_auth do
+    when_i_am_authorized_with_basic_auth
+    and_i_am_signed_in_via_dsi
+    when_i_sign_out
+    then_i_am_redirected_to_the_sign_in_page
+  end
+
+  private
+
+  def when_i_sign_out
+    click_on "Sign out"
+  end
+
+  def then_i_am_redirected_to_the_sign_in_page
+    expect(page).to have_current_path(check_records_sign_in_path)
+  end
+end
