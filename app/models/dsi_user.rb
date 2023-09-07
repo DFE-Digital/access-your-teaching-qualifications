a5a1795674c367c9362782c6f4035225b6f27139
Steve Laing <steve.laing@gmail.com>
2023-09-07 16:08:03 +0100

    Create a DsiUserSession record when creating or updating the user
    We only do this if supplied an optional role. Bypass mechanisisms will still function without a role.

--- app/models/dsi_user.rb
+++ app/models/dsi_user.rb
@@ -3,8 +3,9 @@ class DsiUser < ApplicationRecord
   encrypts :first_name, :last_name
 
   has_many :search_logs
+  has_many :dsi_user_sessions, dependent: :destroy
 
-  def self.create_or_update_from_dsi(dsi_payload)
+  def self.create_or_update_from_dsi(dsi_payload, role = nil)
     dsi_user = find_or_initialize_by(email: dsi_payload.info.fetch(:email))
 
     dsi_user.update!(
@@ -12,6 +13,15 @@ class DsiUser < ApplicationRecord
       last_name: dsi_payload.info.last_name,
       uid: dsi_payload.uid
     )
+
+    if role.present?
+      dsi_user.dsi_user_sessions.create!(
+        role_id: role["id"],
+        role_code: role["code"],
+        organisation_id: dsi_payload.extra.raw_info.organisation.id,
+        organisation_name: dsi_payload.extra.raw_info.organisation.name,
+      )
+    end
 
     dsi_user
   end
