b4a40bc66f5ccd4188356d999c2aae759ca8399b
Steve Laing <steve.laing@gmail.com>
2023-10-17 11:20:30 +0100

    Reset the session on sign in/out controller actions

--- app/controllers/check_records/sign_out_controller.rb
+++ app/controllers/check_records/sign_out_controller.rb
@@ -1,6 +1,7 @@
 module CheckRecords
   class SignOutController < CheckRecordsController
     skip_before_action :handle_expired_session!
+    before_action :reset_session
 
     def new
       session[:dsi_user_id] = nil if dsi_user_signed_in?
