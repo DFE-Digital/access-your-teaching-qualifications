95ec8c816565263ff422318bca7f640121ad564c
Steve Laing <steve.laing@gmail.com>
2023-10-17 11:20:30 +0100

    Reset the session on sign in/out controller actions

--- app/controllers/check_records/sign_in_controller.rb
+++ app/controllers/check_records/sign_in_controller.rb
@@ -2,6 +2,7 @@ module CheckRecords
   class SignInController < CheckRecordsController
     skip_before_action :authenticate_dsi_user!
     skip_before_action :handle_expired_session!
+    before_action :reset_session, except: :not_authorised
 
     def new
     end
