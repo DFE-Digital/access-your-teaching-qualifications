e7842f69d5c067bb4b7f2523ce095c89bcf47def
Steve Laing <steve.laing@gmail.com>
2023-10-17 12:30:40 +0100

    Ensure oauth failure message is rendered
    We reset the session in the sign in controller in order to prevent csrf errors if an authenticated user visits the sign in path again. This prevents the oauth failure flash message from being rendered.
    This commit triggers the oauth failure message via a request param instead.

--- app/controllers/check_records/sign_in_controller.rb
+++ app/controllers/check_records/sign_in_controller.rb
@@ -3,11 +3,18 @@ module CheckRecords
     skip_before_action :authenticate_dsi_user!
     skip_before_action :handle_expired_session!
     before_action :reset_session, except: :not_authorised
+    before_action :handle_failed_sign_in, only: :new, if: -> { params[:oauth_failure] == "true" }
 
     def new
     end
 
     def not_authorised
+    end
+
+    private
+
+    def handle_failed_sign_in
+      flash.now[:warning] = I18n.t("validation_errors.generic_oauth_failure")
     end
   end
 end
