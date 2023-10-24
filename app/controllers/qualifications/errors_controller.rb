0923cc743e6ecba2645e1e51e519211c859dc00d
Malcolm Baig <git@baig.org>
2023-10-24 10:51:42 +0100

    Give AYTQ and Check separate error controllers
    Rather than sharing a single custom error controller, give each service
    its own. This will allow us to use the correct layout templates and
    helps ensure the content on a given error page is specific to the
    service it is rendered from.

---
+++ app/controllers/qualifications/errors_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+module Qualifications
+  class ErrorsController < QualificationsInterfaceController
+    skip_before_action :verify_authenticity_token
+    skip_before_action :authenticate_user!
+    skip_before_action :handle_expired_token!
+
+    def not_found
+      render 'not_found', status: :not_found
+    end
+
+    def unprocessable_entity
+      render 'unprocessable_entity', status: :unprocessable_entity
+    end
+
+    def internal_server_error
+      render 'internal_server_error', status: :internal_server_error
+    end
+  end
+end
