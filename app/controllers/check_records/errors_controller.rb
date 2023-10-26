2ab33b048a7921558f00c6c344c5e3188b25a567
Malcolm Baig <git@baig.org>
2023-10-26 10:36:18 +0100

    Extract common hooks and actions from error controllers

--- app/controllers/check_records/errors_controller.rb
+++ app/controllers/check_records/errors_controller.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 module CheckRecords
   class ErrorsController < CheckRecordsController
-    skip_before_action :verify_authenticity_token
+    include HttpErrorHandling
+
     skip_before_action :authenticate_dsi_user!
     skip_before_action :handle_expired_session!
-
-    def not_found
-      render 'not_found', status: :not_found
-    end
-
-    def unprocessable_entity
-      render 'unprocessable_entity', status: :unprocessable_entity
-    end
-
-    def internal_server_error
-      render 'internal_server_error', status: :internal_server_error
-    end
   end
 end
