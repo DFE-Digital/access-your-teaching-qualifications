d8faf4291dcc91a372bf2919934c08d21419b411
Steve Laing <steve.laing@gmail.com>
2023-10-30 10:44:32 +0000

    Remove Staff dependencies on Devise
    Staff will be authenticated and authorised using DSI.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,17 +1,2 @@
 class Staff < ApplicationRecord
-  devise(
-    :confirmable,
-    :database_authenticatable,
-    :invitable,
-    :lockable,
-    :recoverable,
-    :rememberable,
-    :timeoutable,
-    :trackable,
-    :validatable
-  )
-
-  def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,6 @@ require "route_constraints/access_your_teaching_qualifications_constraint"
 require "route_constraints/check_records_constraint"
 
 Rails.application.routes.draw do
-  devise_for :staff,
-             controllers: {
-               confirmations: "staff/confirmations",
-               invitations: "staff/invitations",
-               passwords: "staff/passwords",
-               sessions: "staff/sessions",
-               unlocks: "staff/unlocks"
-             }
-
-  devise_scope :staff do
-    authenticate :staff do
-      mount Sidekiq::Web, at: "sidekiq"
-    end
-
-    get "/staff/sign_out", to: "staff/sessions#destroy"
-  end
-
   namespace :support_interface, path: "/support" do
     get "/", to: "support_interface#index"
     root to: "support_interface#index", as: :staff_root
