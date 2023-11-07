508a769889cf0e1ef774feaa2c31185639361283
Malcolm Baig <git@baig.org>
2023-11-07 10:28:18 +0000

    Make Check static pages use two thirds layout

--- app/controllers/check_records/static_controller.rb
+++ app/controllers/check_records/static_controller.rb
@@ -2,6 +2,8 @@
 
 module CheckRecords
   class StaticController < CheckRecordsController
+    layout "check_records_two_thirds"
+
     skip_before_action :authenticate_dsi_user!
     skip_before_action :handle_expired_session!
   end
