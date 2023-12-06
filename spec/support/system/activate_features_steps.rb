d472dabf927119785d544dae4b903f5103b7fe89
Graham Pengelly <graham@three-tier.com>
2023-12-06 12:23:21 +0000

    Remove staff basic auth strategy
    This is uneccessary now that we are using DSI for support as it was only
    used to allow an invitation to be sent for the first staff user.

--- spec/support/system/activate_features_steps.rb
+++ spec/support/system/activate_features_steps.rb
@@ -10,10 +10,4 @@ module ActivateFeaturesSteps
   def given_the_support_service_is_open
     FeatureFlags::FeatureFlag.activate(:support_service_open)
   end
-
-  def given_staff_http_basic_is_active
-    FeatureFlags::FeatureFlag.activate(:staff_http_basic_auth)
-  end
-
-  alias_method :and_staff_http_basic_is_active, :given_staff_http_basic_is_active
 end
