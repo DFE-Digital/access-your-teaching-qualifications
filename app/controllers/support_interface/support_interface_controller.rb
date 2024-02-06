2d2cbace24fbda2711b2b2dd2a90f88ec2a788ca
Steve Laing <steve.laing@gmail.com>
2024-02-06 13:53:38 +0000

    Remove DsiUser.staff scope and dependents

--- app/controllers/support_interface/support_interface_controller.rb
+++ app/controllers/support_interface/support_interface_controller.rb
@@ -13,7 +13,10 @@ module SupportInterface
     layout "support_layout"
 
     def current_staff
-      @current_staff ||= DsiUser.staff.find(session[:dsi_user_id]) if session[:dsi_user_id]
+      @current_staff ||= if session[:dsi_user_id]
+        user = DsiUser.find(session[:dsi_user_id])
+        user if user.internal?
+      end
     end
     helper_method :current_staff
 
--- app/models/dsi_user.rb
+++ app/models/dsi_user.rb
@@ -5,16 +5,13 @@ class DsiUser < ApplicationRecord
   has_many :search_logs
   has_many :dsi_user_sessions, dependent: :destroy
 
-  scope :staff, -> { where(staff: true) }
-
-  def self.create_or_update_from_dsi(dsi_payload, role: nil, staff: false)
+  def self.create_or_update_from_dsi(dsi_payload, role: nil)
     dsi_user = find_or_initialize_by(email: dsi_payload.info.fetch(:email))
 
     dsi_user.update!(
       first_name: dsi_payload.info.first_name,
       last_name: dsi_payload.info.last_name,
       uid: dsi_payload.uid,
-      staff:,
     )
 
     if role.present?
@@ -27,6 +24,14 @@ class DsiUser < ApplicationRecord
     end
 
     dsi_user
+  end
+
+  def internal?
+    DfESignIn.bypass? || current_session&.role_code == ENV.fetch("DFE_SIGN_IN_API_INTERNAL_USER_ROLE_CODE")
+  end
+
+  def current_session
+    dsi_user_sessions.order(created_at: :desc).first
   end
 
   def last_sign_in_at
