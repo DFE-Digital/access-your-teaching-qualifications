bd36022b37526db9f5ed6ef21221238e183149e5
Steve Laing <steve.laing@gmail.com>
2024-02-22 10:29:09 +0000

    Add Role#internal field to Support UI forms

--- app/controllers/support_interface/roles_controller.rb
+++ app/controllers/support_interface/roles_controller.rb
@@ -33,8 +33,9 @@ module SupportInterface
     private
 
     def role_params
-      params.require(:role).permit(:code, :enabled ).tap do |rp|
+      params.require(:role).permit(:code, :enabled, :internal).tap do |rp|
         rp[:enabled] = ActiveRecord::Type::Boolean.new.cast(rp[:enabled])
+        rp[:internal] = ActiveRecord::Type::Boolean.new.cast(rp[:internal])
       end
     end
   end
