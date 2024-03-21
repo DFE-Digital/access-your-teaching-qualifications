3a3805fa680ae68328ef84097e3dbcc464afa97d
Malcolm Baig <git@baig.org>
2024-03-21 10:42:54 +0000

    Change AYTQ sign out flow to fix prefetch bug
    We've had reports of users being signed out immediately after sign in,
    likely because of link prefetching in the browser hitting the sign out
    link.
    
    Sign out should probably be a POST request anyway, so rework the flow as
    follows:
    
    - Change the sign out link to be a GET request to a confirmation page
    - Have the confirmation page POST the user confirmation, resulting in
      session deletion
    - Redirect the user to a sign out complete page from where they can
      navigate back to the service start page

--- app/controllers/qualifications/users/sign_out_controller.rb
+++ app/controllers/qualifications/users/sign_out_controller.rb
@@ -2,14 +2,21 @@ module Qualifications
   module Users
     class SignOutController < QualificationsInterfaceController
       skip_before_action :handle_expired_token!
+      skip_before_action :authenticate_user!, only: :complete
 
       def new
+      end
+
+      def create
         if user_signed_in?
           reset_session
           redirect_to "/qualifications/users/auth/identity/logout"
         else
           redirect_to qualifications_start_path
         end
+      end
+
+      def complete
       end
     end
   end
