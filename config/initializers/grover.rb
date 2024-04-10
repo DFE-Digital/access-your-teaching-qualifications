799dcaa87ca7de0a4cec1da684d82ce3b516de69
Steve Laing <steve.laing@gmail.com>
2024-04-10 13:53:43 +0100

    Configure grover

---
+++ config/initializers/grover.rb
@@ -0,0 +1,13 @@
+Grover.configure do |config|
+  config.options = {
+    format: "A4",
+    margin: {
+      top: "0",
+      bottom: "0",
+      left: "0",
+      right: "0"
+    }
+  }
+  config.use_jpeg_middleware = false
+  config.use_png_middleware = false
+end
