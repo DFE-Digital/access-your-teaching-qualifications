3afbbe163de047e8f19aebe7ce42111a0deea82b
Steve Laing <steve.laing@gmail.com>
2024-04-10 13:54:46 +0100

    Render certificate as PDF

--- app/controllers/qualifications/certificates_controller.rb
+++ app/controllers/qualifications/certificates_controller.rb
@@ -1,26 +1,40 @@
 module Qualifications
   class CertificatesController < QualificationsInterfaceController
-    def show
-      client =
-        QualificationsApi::Client.new(token: session[:identity_user_token])
-      certificate =
-        client.certificate(
-          name: current_user.name,
-          type: certificate_type,
-          url: params[:certificate_url]
-        )
+    layout "certificate"
 
-      if certificate
-        send_data certificate.file_data,
-                  filename: certificate.file_name,
-                  content_type: "application/pdf"
-      else
+    def show
+      unless render_certificate?
         redirect_to qualifications_dashboard_path,
                     alert: "Certificate not found"
       end
     end
 
     private
+
+    def client
+      @client ||= QualificationsApi::Client.new(token: session[:identity_user_token])
+    end
+
+    def teacher
+      @teacher ||= client.teacher
+    end
+
+    def qualification
+      @qualification ||= teacher.qualifications.find { |q| q.type == certificate_type.to_sym }
+    end
+
+    def render_certificate?
+      return if qualification.blank?
+
+      case certificate_type.to_sym
+      when :induction
+        teacher.passed_induction?
+      when :qts
+        teacher.qts_awarded?
+      else
+        qualification.awarded_at.present?
+      end
+    end
 
     def certificate_type
       if params[:id].to_sym.in? QualificationsApi::Certificate::VALID_TYPES
