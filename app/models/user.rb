ccea384634eeb2af7df438804130a1a75e1e02a6
Steve Laing <steve.laing@gmail.com>
2024-04-15 16:29:52 +0100

    Amend User model to store new oauth2 credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   encrypts :email, deterministic: true
   encrypts :family_name, :given_name, :name
 
-  def self.from_identity(auth_data)
+  def self.from_auth(auth_data)
     email = auth_data.info.email
     user = find_or_initialize_by(email:)
     user.assign_attributes(
@@ -11,7 +11,8 @@ class User < ApplicationRecord
       given_name: auth_data.info.first_name,
       name: auth_data.info.name,
       trn: auth_data.extra.raw_info.trn,
-      identity_uuid: auth_data.uid
+      auth_uuid: auth_data.uid,
+      auth_provider: auth_data.provider
     )
     user.tap(&:save!)
   end
