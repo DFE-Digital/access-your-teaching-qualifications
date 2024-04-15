0d6a781e031c2c088556bccead24bade62551e0d
Steve Laing <steve.laing@gmail.com>
2024-04-15 16:31:55 +0100

    Enable GOVUK One Login sign in depending on feature flag

---
+++ spec/system/qualifications/user_signs_in_via_onelogin_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "GOVUK One Login auth", type: :system do
+  include CommonSteps
+  include QualificationAuthenticationSteps
+
+  scenario "User signs in via One Login", test: %i[with_stubbed_auth with_fake_quals_api] do
+    given_the_qualifications_service_is_open
+    given_onelogin_authentication_is_active
+    and_onelogin_auth_is_mocked
+    when_i_go_to_the_sign_in_page
+    and_click_the_onelogin_sign_in_button
+    then_i_am_signed_in_after_successfully_authenticating_with_onelogin
+    and_event_tracking_is_working
+  end
+
+  private
+
+  def then_i_am_signed_in_after_successfully_authenticating_with_onelogin
+    expect(User.last.email).to eq "test@example.com"
+  end
+end
