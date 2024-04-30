b5cbce35a23a1ba0e56feb4a50b0f488b7032d28
Malcolm Baig <git@baig.org>
2024-04-30 08:16:18 +0000

    Add feature flag check to One Login user controller

--- app/controllers/qualifications/one_login_users_controller.rb
+++ app/controllers/qualifications/one_login_users_controller.rb
@@ -1,6 +1,16 @@
 module Qualifications
   class OneLoginUsersController < QualificationsInterfaceController
+    before_action :redirct_to_root_unless_one_login_enabled
+
     def show
+    end
+
+    private
+
+    def redirct_to_root_unless_one_login_enabled
+      unless FeatureFlag::Features.active?(:one_login)
+        redirect_to qualifications_root_path
+      end
     end
   end
 end
