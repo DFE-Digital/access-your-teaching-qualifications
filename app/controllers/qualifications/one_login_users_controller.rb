c50196b1e7c4760a09b584b9a5477e2ac30b6a26
Malcolm Baig <git@baig.org>
2024-05-01 13:56:23 +0000

    Fix feature flag check on One Login account page

--- app/controllers/qualifications/one_login_users_controller.rb
+++ app/controllers/qualifications/one_login_users_controller.rb
@@ -1,14 +1,14 @@
 module Qualifications
   class OneLoginUsersController < QualificationsInterfaceController
-    before_action :redirct_to_root_unless_one_login_enabled
+    before_action :redirect_to_root_unless_one_login_enabled
 
     def show
     end
 
     private
 
-    def redirct_to_root_unless_one_login_enabled
-      unless FeatureFlag::Features.active?(:one_login)
+    def redirect_to_root_unless_one_login_enabled
+      unless FeatureFlags::FeatureFlag.active?(:one_login)
         redirect_to qualifications_root_path
       end
     end
