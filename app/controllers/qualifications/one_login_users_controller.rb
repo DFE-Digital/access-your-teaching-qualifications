aa32299b1af4929feb8da097079e177f2417c946
Malcolm Baig <git@baig.org>
2024-05-03 13:31:41 +0000

    Make teacher API data available on One Login account page
    Include PendingDetailChanges in the payload.

--- app/controllers/qualifications/one_login_users_controller.rb
+++ app/controllers/qualifications/one_login_users_controller.rb
@@ -3,6 +3,9 @@ module Qualifications
     before_action :redirect_to_root_unless_one_login_enabled
 
     def show
+      # TODO: add error handling similar to QualificationsController
+      client = QualificationsApi::Client.new(token: session[user_token_session_key])
+      @teacher = client.teacher
     end
 
     private
