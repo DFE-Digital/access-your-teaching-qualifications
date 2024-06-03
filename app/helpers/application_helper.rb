20e9a7cd438fc45025609e03fa714c229b947cfd
Malcolm Baig <git@baig.org>
2024-06-03 14:36:04 +0000

    Refactor navigation code to fix error pages
    AYTQ 404 pages (and other HTTP error pages) aren't rendering properly
    because of a bug in the navigation helper. Summary:
    
    - The error pages sit in routes without a service-specific namespace, eg -
      http://hostname.tld/404
    - The navigation helper relies on a 'qualifications' namespace being
      present to render the AYTQ nav
    - If this isn't present, the helper attempts to render the support nav
      instead
    - This fails because there isn't a valid dsi_user signed in (but even if
      there was this would be the wrong thing to do regardless)
    
    Fix this by doing the following:
    
    - Extract a Qualifications::NavigationComponent out of the navigation
      helper method
    - Update the base layout to allow the navigation to be specified in a
      content_for block
    - Have the AYTQ layout use the new nav component, while the Support
      layout continues to use the helper method
    
    This leaves us in the following state:
    
    - AYTQ uses its own navigation component, removing the logical check and
      thus fixing the bug described above.
    - CTR remains unchanged as it was using its own nav component already.
    - Support renders its nav with a renamed helper method, although this
      should probably be extracted into a component at some point as well.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,44 +3,34 @@ module ApplicationHelper
     request.path.split("/").second
   end
 
-  def navigation
-    govuk_header(service_name: t("service.name")) do |header|
-      if current_namespace == "qualifications"
-        if current_user
+  # TODO: refactor Support navigation into a component
+  def support_navigation
+    govuk_header(service_name: "AYTQ / CTR Support") do |header|
+      if current_dsi_user.internal?
+        header.with_navigation_item(
+          active: current_page?(main_app.support_interface_feature_flags_path),
+          href: main_app.support_interface_feature_flags_path,
+          text: "Features"
+        )
+        header.with_navigation_item(
+          active: request.path.start_with?("/support/feedback"),
+          text: "Feedback",
+          href: main_app.support_interface_feedback_index_path
+        )
+        if FeatureFlags::FeatureFlag.active?(:manage_roles)
           header.with_navigation_item(
-            active: current_page?(main_app.qualifications_identity_user_path),
-            href: main_app.qualifications_identity_user_path,
-            text: "Account"
-          )
-          header.with_navigation_item(href: main_app.qualifications_new_sign_out_path, text: "Sign out")
-        end
-      else
-        if current_dsi_user.internal?
-          header.with_navigation_item(
-            active: current_page?(main_app.support_interface_feature_flags_path),
-            href: main_app.support_interface_feature_flags_path,
-            text: "Features"
-          )
-          header.with_navigation_item(
-            active: request.path.start_with?("/support/feedback"),
-            text: "Feedback",
-            href: main_app.support_interface_feedback_index_path
-          )
-          if FeatureFlags::FeatureFlag.active?(:manage_roles)
-            header.with_navigation_item(
-              active: request.path.start_with?("/support/roles"),
-              text: "Check role codes",
-              href: main_app.support_interface_roles_path
+            active: request.path.start_with?("/support/roles"),
+            text: "Check role codes",
+            href: main_app.support_interface_roles_path
 
-            )
-          end
-        end
-        if current_dsi_user
-          header.with_navigation_item(
-            href: main_app.check_records_dsi_sign_out_path(id_token_hint: session[:id_token]),
-            text: "Sign out"
           )
         end
+      end
+      if current_dsi_user
+        header.with_navigation_item(
+          href: main_app.check_records_dsi_sign_out_path(id_token_hint: session[:id_token]),
+          text: "Sign out"
+        )
       end
     end
   end
