47b30e1ee188c3cc15c55e0041039e1bebe9a4ee
Malcolm Baig <git@baig.org>
2024-07-01 10:28:55 +0000

    Fix terms and conditions logic at session timeout
    When a user session times out, the current terms and conditions
    enforcement in CTR will result in an infinite redirect loop.
    
    This is because if an expired session is detected, we send the user to
    the SignOutController. Here the controller should clear the session and
    send the user to the sign in page.
    
    With the addition of the terms and conditions logic, the sign out
    controller does the enforcement check in a before filter before it gets
    the chance to clear the session.
    
    To fix the redirect loop this creates:
    
    - Add the sign in and sign out paths for CTR to the list of paths the
      terms and conditions enforcement skips
    
    This commit also includes minor refactors and reformatting carried out
    during investigation of the bug.

--- app/controllers/check_records/terms_and_conditions_controller.rb
+++ app/controllers/check_records/terms_and_conditions_controller.rb
@@ -6,8 +6,10 @@ module CheckRecords
     end
 
     def update
-      current_dsi_user.update!(terms_and_conditions_version_accepted: CURRENT_TERMS_AND_CONDITIONS_VERSION,
-                              terms_and_conditions_timestamp: Time.zone.now)
+      current_dsi_user.update!(
+        terms_and_conditions_version_accepted: CURRENT_TERMS_AND_CONDITIONS_VERSION,
+        terms_and_conditions_timestamp: Time.zone.now
+      )
       redirect_to check_records_root_path, notice: "Terms and conditions accepted"
     end
   end
