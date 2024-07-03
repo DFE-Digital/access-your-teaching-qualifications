bd1272844edfe1060c4f15536d105adf043dd720
Malcolm Baig <git@baig.org>
2024-07-03 11:59:07 +0000

    Remove reset_session from CheckRecords::SignInController
    Not clear why we were doing this in the first place, and it removes the
    ability to set a flash message when redirecting to this page.
    
    Remove for now and monitor.

--- app/controllers/check_records/sign_in_controller.rb
+++ app/controllers/check_records/sign_in_controller.rb
@@ -2,7 +2,6 @@ module CheckRecords
   class SignInController < CheckRecordsController
     skip_before_action :authenticate_dsi_user!
     skip_before_action :handle_expired_session!
-    before_action :reset_session, except: :not_authorised
     before_action :handle_failed_sign_in, only: :new, if: -> { params[:oauth_failure] == "true" }
 
     def new
