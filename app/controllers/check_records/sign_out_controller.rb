142cee4bf28c2176d302e87fd02f2bbf49c93bdc
Felix Clack <felixclack@gmail.com>
2024-07-17 11:03:45 +0100

    Add better messaging for session expiry
    When a session expires, we sign the user out and redirect them to the
    start page with no explanation.
    
    This change introduces a flash message with a small explanation in an
    attempt to make the scenario clearer.
    
    I also modified the session reset to only remove the user information
    rather than clobbering the whole session. This ensures that flash
    messages are persisted across redirects.

--- app/controllers/check_records/sign_out_controller.rb
+++ app/controllers/check_records/sign_out_controller.rb
@@ -2,9 +2,9 @@ module CheckRecords
   class SignOutController < CheckRecordsController
     skip_before_action :handle_expired_session!
     skip_before_action :enforce_terms_and_conditions_acceptance!
-    before_action :reset_session
 
     def new
+      session.delete(:dsi_user_id)
       redirect_to check_records_sign_in_path
     end
   end
