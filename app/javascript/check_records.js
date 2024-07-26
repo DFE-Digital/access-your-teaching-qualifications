d6722e65b0a8ac6c53c4bf07e4d4463c4147c578
Felix Clack <felixclack@gmail.com>
2024-07-26 11:52:29 +0100

    Add a link to print the page
    We want to make it easier for a user to print the page.
    
    Adding a link that triggers the browser print functionality will make it
    clearer that this is a printable page.

--- app/javascript/check_records.js
+++ app/javascript/check_records.js
@@ -1,3 +1,15 @@
 import { initAll } from "govuk-frontend";
 
 initAll();
+
+function printPage() {
+  window.print();
+}
+
+// Add event listener to the print button
+document.addEventListener("DOMContentLoaded", () => {
+  const printButton = document.querySelector("[data-print-button]");
+  if (printButton) {
+    printButton.addEventListener("click", printPage);
+  }
+});
