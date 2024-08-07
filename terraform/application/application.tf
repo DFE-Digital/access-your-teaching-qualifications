23488aa46d74dbc602d693db3ebd790e97ef9902
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-08-07 17:28:38 +0100

    Terraform code for deploying to AKS

---
+++ terraform/application/application.tf
@@ -0,0 +1,48 @@
+module "application_configuration" {
+  source = "./vendor/modules/aks//aks/application_configuration"
+
+  namespace              = var.namespace
+  environment            = var.environment
+  azure_resource_prefix  = var.azure_resource_prefix
+  service_short          = var.service_short
+  config_short           = var.config_short
+  secret_key_vault_short = "app"
+
+  is_rails_application = true
+
+  config_variables = {
+    ENVIRONMENT_NAME           = var.environment
+    PGSSLMODE                  = local.postgres_ssl_mode
+    HOSTING_DOMAIN             = "https://${local.access_external_domain}"
+    CHECK_RECORDS_DOMAIN       = "https://${local.check_external_domain}"
+    DFE_SIGN_IN_REDIRECT_URL   = "https://${local.check_external_domain}/check-records/auth/dfe/callback"
+    AZURE_STORAGE_ACCOUNT_NAME = local.evidence_storage_account_name
+    AZURE_STORAGE_CONTAINER    = azurerm_storage_container.uploads.name
+  }
+  secret_variables = {
+    DATABASE_URL             = module.postgres.url
+    REDIS_URL                = module.redis-cache.url
+    AZURE_STORAGE_ACCESS_KEY = azurerm_storage_account.evidence.primary_access_key
+  }
+}
+
+module "web_application" {
+  source = "./vendor/modules/aks//aks/application"
+
+  is_web = true
+
+  namespace    = var.namespace
+  environment  = var.environment
+  service_name = var.service_name
+  probe_path   = "/health"
+
+  cluster_configuration_map  = module.cluster_data.configuration_map
+  kubernetes_config_map_name = module.application_configuration.kubernetes_config_map_name
+  kubernetes_secret_name     = module.application_configuration.kubernetes_secret_name
+
+  docker_image = var.docker_image
+
+  send_traffic_to_maintenance_page = var.send_traffic_to_maintenance_page
+
+  web_external_hostnames = [local.check_domain]
+}
