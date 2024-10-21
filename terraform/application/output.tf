040a3eb0b7f51ef2218f5d47cbe743a0e405a925
Shahe Islam <shahe.islam@education.gov.uk>
2024-10-21 13:24:19 +0100

    Testing smoke test correction

--- terraform/application/output.tf
+++ terraform/application/output.tf
@@ -4,3 +4,11 @@ output "urls" {
     "https://${local.check_domain}"
   ]
 }
+
+output "external_urls" {
+  value = [
+    "https://${local.access_external_domain}",
+    "https://${local.check_external_domain}"
+  ]
+  description = "List of external URLs for the application"
+}
