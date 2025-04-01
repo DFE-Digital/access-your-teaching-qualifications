5696943e47691a1bdd393cfd8dd8d189584eae04
Richard Pattinson <richie.pattinson@googlemail.com>
2025-04-01 17:30:29 +0100

    Fix accessibility issues

--- spec/system/qualifications/user_submits_a_request_to_change_name_spec.rb
+++ spec/system/qualifications/user_submits_a_request_to_change_name_spec.rb
@@ -55,9 +55,7 @@ RSpec.feature "Account page", type: :system do
   end
 
   def and_i_can_see_a_list_of_valid_evidence
-    within(".govuk-details") do
-      expect(page).to have_selector(:css, "li", text: "deed poll", visible: :all)
-    end
+    expect(page).to have_selector(:css, "li", text: "deed poll", visible: :all)
   end
 
   def when_i_submit_the_form
