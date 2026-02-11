35d15055e7e8cd5d4b742cd7caea11c1df827ba8
Naomi Lockhart <naomi@lockyy.com>
2026-02-11 14:32:45 +0000

    Add error handling to Qualifications::OneLoginUsersController

--- app/controllers/qualifications/one_login_users_controller.rb
+++ app/controllers/qualifications/one_login_users_controller.rb
@@ -3,9 +3,41 @@ module Qualifications
     before_action :redirect_to_root_unless_one_login_active
 
     def show
-      # TODO: add error handling similar to QualificationsController
       client = QualificationsApi::Client.new(token: current_session.user_token)
       @teacher = client.teacher
+    rescue QualificationsApi::InvalidTokenError
+      redirect_to qualifications_sign_out_path and return
+    rescue QualificationsApi::ForbiddenError => e
+      send_additional_detail_to_sentry(e)
+      redirect_to qualifications_sign_out_path and return
+    end
+
+    private
+
+    def extract_payload(token)
+      _header, encoded_payload, _signature = token.split('.')
+      payload = Base64.urlsafe_decode64(encoded_payload)
+      JSON.parse payload
+    end
+
+    def send_additional_detail_to_sentry(exception)
+      api_token_payload = extract_payload(current_session.user_token)
+      Sentry.with_scope do |scope|
+        scope.set_user(id: current_user.id)
+        scope.set_context(
+          'Authentication session',
+          {
+            trn: current_user.trn,
+            auth_uuid: current_user.auth_uuid,
+            api_tkn_expiry: current_session.session_expiry_time,
+            "payload.trn" => api_token_payload["trn"],
+            "payload.scope" => api_token_payload["scope"],
+            "payload.identity_exp" => api_token_payload["exp"],
+            "payload.identity_exp_parsed" => Time.zone.at(api_token_payload["exp"]),
+          }
+        )
+        Sentry.capture_exception(exception)
+      end
     end
   end
 end
