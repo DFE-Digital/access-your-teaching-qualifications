e3c3b07d29fcd032135823921c002d80359c1f28
Naomi Lockhart <naomi@lockyy.com>
2026-04-17 16:25:02 +0100

    Add healthcheck for database table presence

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,9 +1,11 @@
 require_relative "../../lib/ok_computer_checks/notify_check"
+require_relative "../../lib/ok_computer_checks/database_integrity_check"
 
 OkComputer.logger = Rails.logger
 OkComputer.mount_at = "health"
 
 OkComputer::Registry.register "postgresql", OkComputer::ActiveRecordCheck.new
+OkComputer::Registry.register "database_integrity", OkComputerChecks::DatabaseIntegrityCheck.new
 OkComputer::Registry.register "version", OkComputer::AppVersionCheck.new
 OkComputer::Registry.register "notify", OkComputerChecks::NotifyCheck.new
 
